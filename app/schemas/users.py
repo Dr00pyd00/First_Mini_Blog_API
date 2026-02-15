d96387209adb8cdf3f50f212b2bcda38d5881db4
Luna <kottakcode@gmail.com>
2026-02-15 19:25:52 +0300

    schemas user/UserCreateSchema optimized with Field and methods

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,12 +1,41 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
 from datetime import datetime
+import re
 
 
 # Create a user:
 class UserCreateSchema(BaseModel):
-    username: str
-    password: str
+    username: str = Field(
+        ...,
+        min_length=3,
+        max_length=50,
+        description="Username: 3 to 50 characters."
+    )
+    password: str = Field(
+        ...,
+        min_length=5,
+        max_length=80,
+        description="user password: 5 to 80 characters"
+    )
     is_active : bool = True
+
+    @field_validator("username")
+    @classmethod
+    def username_alphanumeric(cls, name)->str:
+        if re.match(r'^[a-zA-Z0-9_]+$', name) is None:
+            raise ValueError("<username> must be alphanumeric only (can contain '_').")
+        return name
+
+    @field_validator("password")
+    @classmethod
+    def password_complexity(cls, user_password:str)->str:
+
+        if not any(char.isdigit() for char in user_password):
+            raise ValueError("password must have at least ONE digit.")
+        if not any(char.isalpha() for char in user_password):
+            raise ValueError("password must have at least ONE alphabetic symbol.")
+
+        return user_password
 
 
 #  Data client can see:
